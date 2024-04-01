8a66461d7d2b722b8100931d982d3192a4d2cc25
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-04-01 22:23:01 +0300

    InspimoEticaret

---
+++ src/components/shopping-cards/shopping-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping-cards.component.html',
+  styleUrl: './shopping-cards.component.css'
+})
+export class ShoppingCardsComponent {
+
+}
