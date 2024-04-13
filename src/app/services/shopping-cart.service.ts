0cdf2dc6223bcc42b52feaf5d1bbdaa647e42280
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-04-13 20:43:09 +0300

    ..

--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ShoppingCartModel } from '../models/shopping-cart.model';
+import { HttpClient, HttpClientJsonpModule, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,17 @@ export class ShoppingCartService {
 
   shoppingCarts: ShoppingCartModel[] = [];
   
-  constructor() { }
+  constructor(private _http:HttpClient) {
+    this.getAll()
+   }
+  getAll(){
+   this._http.get<ShoppingCartModel[]>("http://localhost:3000/shoppingCarts").subscribe({
+    next:(res)=>{
+    this.shoppingCarts=res
+    },
+    error:(err:HttpErrorResponse)=>{
+      console.log(err)
+    }
+   })
+  }
 }
